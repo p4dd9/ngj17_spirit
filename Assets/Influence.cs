9a66b5c6149d2fbe7d757fe9ef50cbae36175b3f
Andreas Lang <alang.mmt-b2015@fh-salzburg.ac.at>
2017-04-22 00:36:56 +0200

    add: Spirit movement             pushing GoodStuff             ignoring BadStuff

---
+++ Assets/Influence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Influence : MonoBehaviour
+{
+    public float PushForce = 5f;
+
+	//// Use this for initialization
+	//void Start ()
+ //   {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update ()
+ //   {
+		
+	//}
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "GoodStuff")
+        {
+            Rigidbody2D otherRB = other.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 dir = other.gameObject.transform.position - gameObject.transform.position;
+            otherRB.AddForce(dir.normalized * PushForce, ForceMode2D.Impulse);
+        }
+    }
+}
