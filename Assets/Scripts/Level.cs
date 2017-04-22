8bf6b19167a5481d53a2987940f8afa5ad22b896
Martin Treacy-Schwartz <mts@gameanalytics.com>
2017-04-22 16:25:25 +0200

    finished level 1

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        GameManager.Instance.currentGameState = EGameState.InGame;
+    }
+}
