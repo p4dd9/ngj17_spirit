af280877e21b5b09d64789773a7d64fe37ee8394
Martin Treacy-Schwartz <mts@gameanalytics.com>
2017-04-22 14:13:04 +0200

    added game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum EGameState
+{
+    Menu,
+    InGame,
+    GameOver
+}
+
+public class GameManager : Singleton<GameManager>
+{
+    public EGameState currentGameState = EGameState.Menu;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+		
+
+        if(this.currentGameState == EGameState.Menu)
+        {
+            if (Input.anyKey)
+            {
+                this.currentGameState = EGameState.InGame;
+                SceneManager.LoadScene("Level1");
+            }
+        }
+        else if (this.currentGameState == EGameState.InGame)
+        {
+            if (Input.GetKeyUp(KeyCode.G))
+            {
+                Debug.Log("Pressed G");
+                GameObject gameOverGO = Resources.Load<GameObject>("GameOver");
+                Instantiate(gameOverGO);
+                this.currentGameState = EGameState.GameOver;
+            }
+        }
+        else if(this.currentGameState == EGameState.GameOver)
+        {
+            if(Input.anyKey)
+            {
+                this.currentGameState = EGameState.Menu;
+                SceneManager.LoadScene("Menu");
+            }
+        }
+	}
+}
