a0581fdd30d99d5d0ed6bcfb7a93bee37a50adb1
Andreas Lang <alang.mmt-b2015@fh-salzburg.ac.at>
2017-04-22 21:40:10 +0200

    add: gravitational force between GoodStuff objects

---
+++ Assets/Scripts/GravitationalForce.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationalForce : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == transform.parent.tag)
+        {
+            Rigidbody2D otherRB = other.gameObject.GetComponentInParent<Rigidbody2D>();
+
+            Vector3 dir = transform.position - other.gameObject.transform.position;
+
+            if(otherRB)
+                otherRB.AddForce(dir, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == transform.parent.tag)
+        {
+            Rigidbody2D otherRB = other.gameObject.GetComponentInParent<Rigidbody2D>();
+
+            Vector3 dir = transform.position - other.gameObject.transform.position;
+
+            if (otherRB)
+                otherRB.AddForce(dir);
+        }
+    }
+}
