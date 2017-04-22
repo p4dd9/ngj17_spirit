636f8eec6ab9b35e5591f8b0cf29cfe18bf1270a
Andreas Lang <alang.mmt-b2015@fh-salzburg.ac.at>
2017-04-22 15:42:44 +0200

    mod EnemyAction.cs implemented split action     Merging.cs objects now don't merge if the already have max size
    add prefabs for split and consume enemy

--- Assets/Scripts/EnemyAction.cs
+++ Assets/Scripts/EnemyAction.cs
@@ -11,6 +11,8 @@ public class EnemyAction : MonoBehaviour
     }
 
     public Attack AttackBehavior = Attack.Consume;
+    public float PushForce = 2f;
+    public GameObject GoodStuffPrefab;
 
 	// Use this for initialization
 	void Start ()
@@ -35,13 +37,52 @@ public class EnemyAction : MonoBehaviour
                     {
                         Destroy(other.gameObject);
                     }
+                    else
+                    {
+                        Destroy(gameObject);
+                    }
                     break;
 
                 case Attack.Split:
+                    if (other.gameObject.transform.localScale.x > other.gameObject.GetComponent<Merging>().minSize)
+                    {
+                        Rigidbody2D otherRB = other.gameObject.GetComponent<Rigidbody2D>();
+                        Transform oldTransform = other.gameObject.transform;
+                        Destroy(other.gameObject);
+
+                        if(GoodStuffPrefab != null)
+                        {
+                            float newScal = oldTransform.localScale.x / 2f;
+                            Vector3 dir = new Vector3(newScal, newScal, 0);
+                            GameObject split1 = Instantiate(GoodStuffPrefab, oldTransform.position - dir, Quaternion.identity);
+                            GameObject split2 = Instantiate(GoodStuffPrefab, oldTransform.position + dir, Quaternion.identity);
+
+                            // update scale, mass and drag
+                            split1.transform.localScale = Vector3.one * newScal;
+                            Rigidbody2D split1RB = split1.GetComponent<Rigidbody2D>();
+                            split1RB.mass = otherRB.mass / 2f;
+                            split1RB.drag = otherRB.drag / 2f;
+
+                            split2.transform.localScale = Vector3.one * newScal;
+                            Rigidbody2D split2RB = split2.GetComponent<Rigidbody2D>();
+                            split2RB.mass = otherRB.mass / 2f;
+                            split2RB.drag = otherRB.drag / 2f;
+
+                            // push the 2 new objects away
+                            split1.GetComponent<Rigidbody2D>().AddForce(-dir * PushForce, ForceMode2D.Impulse);
+                            split2.GetComponent<Rigidbody2D>().AddForce(dir * PushForce, ForceMode2D.Impulse);
+                        }
+                    }
                     break;
             }
 
         }
     }
+
+    //private void adjustRigidbody (GameObject other)
+    //{
+    //    Rigidbody2D otherRB = other.GetComponent<Rigidbody2D>();
+    //    otherRB.mass
+    //}
 
 }
--- Assets/Scripts/FallOfScreen.cs
+++ Assets/Scripts/FallOfScreen.cs
@@ -9,6 +9,7 @@ public class FallOfScreen : MonoBehaviour
     float bottomConstraint = 0;
     float topConstraint = 0; 
     float distanceZ;
+    float buffer;
 
     void Start()
     {
@@ -18,24 +19,26 @@ public class FallOfScreen : MonoBehaviour
         rightConstraint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, distanceZ)).x;
         bottomConstraint = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, distanceZ)).y;
         topConstraint = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, Screen.height, distanceZ)).y;
+
+        buffer = transform.localScale.x;
     }
 
     void LateUpdate()
     {
-        if (transform.position.x < leftConstraint)
+        if (transform.position.x < leftConstraint - buffer)
         {
             transform.position = new Vector3(rightConstraint, transform.position.y, transform.position.z);
         }
-        else if (transform.position.x > rightConstraint)
+        else if (transform.position.x > rightConstraint + buffer)
         {
             transform.position = new Vector3(leftConstraint, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.y < bottomConstraint)
+        if (transform.position.y < bottomConstraint - buffer)
         {
             transform.position = new Vector3(transform.position.x, topConstraint, transform.position.z);
         }
-        else if (transform.position.y > topConstraint)
+        else if (transform.position.y > topConstraint + + buffer)
         {
             transform.position = new Vector3(transform.position.x, bottomConstraint, transform.position.z);
         }
