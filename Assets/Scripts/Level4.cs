175cc349c7b7e086e07b3ccba401a9bff61c651f
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2017-04-23 01:17:25 +0200

    multiplayer level

--- Assets/Scripts/Level4.cs
+++ Assets/Scripts/Level4.cs
@@ -5,42 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class Level4 : Level
 {
-    public float completeLevelWaitTimeInSecs = 2;
-    private bool completeLevelTriggered;
+	public float completeLevelWaitTimeInSecs = 2;
+	private bool completeLevelTriggered;
 	private string levelName = "Level5";
 
-    private void Start()
-    {
-        StartCoroutine(this.LevelCompleteCheck());
-    }
+	private void Start ()
+	{
+		StartCoroutine (this.LevelCompleteCheck ());
+	}
 
-    private IEnumerator CompleteLevel()
-    {
-        yield return new WaitForSeconds(this.completeLevelWaitTimeInSecs);
-		SceneManager.LoadScene(levelName);
-    }
+	private IEnumerator CompleteLevel ()
+	{
+		yield return new WaitForSeconds (this.completeLevelWaitTimeInSecs);
+		SceneManager.LoadScene (levelName);
+	}
 
-    public override void TriggerCompleteLevel()
-    {
-        if (!this.completeLevelTriggered)
-        {
-            this.completeLevelTriggered = true;
-            base.TriggerCompleteLevel();
-            StartCoroutine(this.CompleteLevel());
-        }
-    }
+	public override void TriggerCompleteLevel ()
+	{
+		if (!this.completeLevelTriggered) {
+			this.completeLevelTriggered = true;
+			base.TriggerCompleteLevel ();
+			StartCoroutine (this.CompleteLevel ());
+		}
+	}
 
-    private IEnumerator LevelCompleteCheck()
-    {
-        if(FindObjectsOfType<EnemyAction>().Length == 0)
-        {
-            TriggerCompleteLevel();
-        }
-        else
-        {
-            yield return new WaitForSeconds(1);
-            StartCoroutine(this.LevelCompleteCheck());
-        }
+	private IEnumerator LevelCompleteCheck ()
+	{
+		if (FindObjectsOfType<EnemyAction> ().Length == 0) {
+			TriggerCompleteLevel ();
+		} else {
+			yield return new WaitForSeconds (1);
+			StartCoroutine (this.LevelCompleteCheck ());
+		}
         
-    }
+	}
 }
