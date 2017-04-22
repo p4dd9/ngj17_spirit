261485947f33bb2bc097bfdcbeaf3b0e25162338
Andreas Lang <alang.mmt-b2015@fh-salzburg.ac.at>
2017-04-22 14:15:56 +0200

    add random movement for enemies

---
+++ Assets/Scripts/EnemyAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAction : MonoBehaviour
+{
+    public enum Attack
+    {
+        Consume,
+        Split
+    }
+
+    public Attack Behavior = Attack;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "GoodStuff")
+        {
+            if(other.gameObject.transform.localScale.x < transform.localScale.x)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float WanderDistance = 2f;
+    public float WanderRadius = 5f;
+    public float WanderRate = 1f;
+
+    private Rigidbody2D shapeRB;
+    private Vector2 movement = Vector2.zero;
+    private float timer = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        shapeRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(timer >= WanderRate)
+        {
+            movement = wander();
+            timer = 0f;
+        }
+
+        timer += Time.deltaTime;
+       
+	}
+
+    private void FixedUpdate()
+    {
+        shapeRB.AddForce(movement * Speed);
+    }
+
+    Vector2 wander()
+    {
+        Vector2 dir = Random.insideUnitCircle * WanderRadius;
+
+        return dir * WanderDistance;
+    }
+}
