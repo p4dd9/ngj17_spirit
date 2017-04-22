ae1482d2b147386fdd9109b06212a7495409eb83
Martin Treacy-Schwartz <mts@gameanalytics.com>
2017-04-22 17:47:55 +0200

    influence component bug fix

--- Assets/Scripts/Influence.cs
+++ Assets/Scripts/Influence.cs
@@ -19,7 +19,7 @@ public class Influence : MonoBehaviour
     {
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "GoodStuff")
         {
