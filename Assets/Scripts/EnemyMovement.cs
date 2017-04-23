d7a181a2edb444134f030d2c4ff81afe0c3c8596
Martin Treacy-Schwartz <mts@gameanalytics.com>
2017-04-23 12:17:05 +0200

    updated enemy movement behaviour

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,42 +5,48 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float Speed = 5f;
-    public float WanderDistance = 2f;
-    public float WanderRadius = 5f;
     public float WanderRate = 1f;
+    public float directionLagCoefficient = 2;
 
     private Rigidbody2D shapeRB;
     private Vector2 movement = Vector2.zero;
     private float timer = 0f;
+    private Vector2 currentDirection;
+    private Vector2 desiredDirection;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         shapeRB = GetComponent<Rigidbody2D>();
-	}
+        desiredDirection = wander();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         if(timer >= WanderRate)
         {
-            movement = wander();
+            desiredDirection = wander();
             timer = 0f;
         }
 
         timer += Time.deltaTime;
-       
-	}
+        this.currentDirection = Vector2.Lerp(this.currentDirection, this.desiredDirection.normalized, Time.deltaTime * directionLagCoefficient);
+    }
 
     private void FixedUpdate()
     {
-        shapeRB.AddForce(movement * Speed * transform.localScale.x);
+        shapeRB.AddForce(this.currentDirection.normalized * this.currentDirection.magnitude * Speed);
     }
 
     Vector2 wander()
     {
-        Vector2 dir = Random.insideUnitCircle * WanderRadius;
+        //Vector2 dir = Random.insideUnitCircle * WanderRadius;
 
-        return dir * WanderDistance;
+        //return dir * WanderDistance;
+        float inputX = Random.Range(-1.0f, 1.0f);
+        float inputY = Random.Range(-1.0f, 1.0f);
+
+        return new Vector2(inputX, inputY);
     }
 }
